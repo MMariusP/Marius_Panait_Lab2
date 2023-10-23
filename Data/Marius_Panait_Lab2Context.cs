4afc36b3f4bbdc7f0eff073ff0972b667018491f
MMariusP <marius_panait@ymail.com>
2023-10-23 20:32:56 +0300

    Add project files.

---
+++ Data/Marius_Panait_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marius_Panait_Lab2.Models;
+
+namespace Marius_Panait_Lab2.Data
+{
+    public class Marius_Panait_Lab2Context : DbContext
+    {
+        public Marius_Panait_Lab2Context (DbContextOptions<Marius_Panait_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Marius_Panait_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Marius_Panait_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Marius_Panait_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marius_Panait_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstNmae { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        public string FullName { get { return this.firstNmae + " " + this.lastName; } }
+
+        public ICollection<Book>? Books { get; set; } //navigation property
+    }
+}
